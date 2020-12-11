c5b9d982b98d8c30d460627554b68412734e4be6
chobits74 <wahidahanuar@Wahidahs-MacBook-Pro.local>
2020-12-11 17:03:42 +0800

    done with hashed pwd and login

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 from flask import Flask, render_template, redirect, url_for, request
 from flask_sqlalchemy import SQLAlchemy 
 from flask_admin import Admin 
-from flask_login import UserMixin, LoginManager
+from flask_login import UserMixin, LoginManager, login_user,login_required, logout_user
 from flask_admin.contrib.sqla import ModelView
-from flask_user import current_user, login_required, roles_required, UserManager, UserMixin
+from flask_user import current_user, roles_required, UserManager
 from datetime import datetime
 from flask_admin import BaseView, expose
 from werkzeug.security import generate_password_hash, check_password_hash
@@ -15,22 +15,23 @@ app.config['SECRET_KEY'] = "secret Admirer"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///student_file.sqlite'
 
-db = SQLAlchemy(app)
+#db = SQLAlchemy(app)
 
 login = LoginManager(app)
+db = SQLAlchemy(app)
 
 @login.user_loader
 def load_user(user_id):
-    return User.query.get(user_id)
+    return User.query.get(int(user_id))
 
 
     # Initialize Flask-SQLAlchemy
-db = SQLAlchemy(app)
+#db = SQLAlchemy(app)
     
 
     # Define the User data-model.
     # NB: Make sure to add flask_user UserMixin !!!
-class User(db.Model, UserMixin):
+class User(UserMixin, db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     active = db.Column('is_active', db.Boolean(), nullable=False, server_default='1')
@@ -48,6 +49,16 @@ class User(db.Model, UserMixin):
         
         # Define the relationship to Role via UserRoles
     roles = db.relationship('Role', secondary='user_roles')
+
+    def __init__(self, email, password, first_name, last_name, reg_number):
+        self.email = email
+        self.password = password
+        self.first_name = first_name
+        self.last_name = last_name
+        self.reg_number = reg_number
+
+    def __repr__(self):
+        return self.first_name
 
     def set_password(self, password):
         self.password = generate_password_hash(password)
@@ -93,9 +104,20 @@ class UserFiles(db.Model):
     
 
     # Setup Flask-User and specify the User data-model
+
 #user_manager = UserManager(app, db, User)
 
-class MyModelView(ModelView):
+class UserView(ModelView):
+    column_exclude_list = []
+    column_display_pk = True
+    can_create = True
+    can_edit = True
+    can_delete = True
+    can_export = True
+
+    def on_model_change(self, form, model, is_created):
+        model.password = generate_password_hash(model.password, method='sha256')
+
     def is_accessible(self):
         return  not current_user.is_authenticated
 
@@ -112,29 +134,41 @@ def index():
 #   @expose('/')
 #  def index(self):
 # #     return self.render(home_index.html)
+@app.route('/login')
+def login():
+    return render_template('login.html')
+    
 @app.route('/login', methods = ['POST'])
 def login():
     email = request.form.get('email')
     password = request.form.get('password')
+    
     checkme = True if request.form.get('checkme') else False
 
     user = User.query.filter_by(email = email).first()
+    login_user(user)
 
     if not user or not check_password_hash(user.password, password):
         flash('Please check your login details and try again.')
         return redirect(url_for('login'))
 
-    login_user(user, checkme=checkme)
-    return render_template('folio.html')
+    #login_user(user, checkme=checkme)
+    return redirect(url_for('folio'))
 
+@app.route('/folio')
+@login_required
+def folio():
+    return render_template('folio.html',first_name = current_user.first_name )
 
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
     
     
-
-
-
 admin = Admin(app, 'genki?', base_template='layout.html', template_mode='bootstrap3')
-admin.add_view(MyModelView(User, db.session))
+admin.add_view(UserView(User, db.session))
 admin.add_view(MyFileView(FileStorage, db.session))
 
     # Create all database tables
